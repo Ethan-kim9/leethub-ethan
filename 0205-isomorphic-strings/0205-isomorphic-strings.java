7384ebda2ff0dea7d8af7c636e0aacfe8bd94a1a
이상(Ethan) <67469416+Ethan-kim9@users.noreply.github.com>
2023-02-03 13:40:53 +0900

    Time: 38 ms (14.17%), Space: 42.3 MB (67.22%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        Map<Character, Character> characterMap = new HashMap<>();
+
+        for(int i=0; i< s.length(); i++){
+            char frontChar = s.charAt(i);
+            char backChar = t.charAt(i);
+
+            if(i==0){
+                characterMap.put(frontChar,backChar);
+            }
+            if (characterMap.containsKey(frontChar)) {
+                if(!characterMap.containsValue(backChar)){
+                    return false;
+                }
+                if(characterMap.get(frontChar) != backChar){
+                    return false;
+                }
+            }else{
+                if(characterMap.containsValue(backChar)){
+                    return false;
+                }
+                characterMap.put(frontChar,backChar);
+            }
+                
+        }
+        return true;
+    }
+}
