dd502b0f969937ce5fb1f59d46364d182de72f8f
이상(Ethan) <67469416+Ethan-kim9@users.noreply.github.com>
2023-02-08 19:56:25 +0900

    Time: 1 ms (15.07%), Space: 43 MB (64.20%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int ret = -1;
+        for(int i=0; i<nums.length; i++){
+            if(target == nums[i])ret=i;
+        }
+        return ret;
+    }
+}
